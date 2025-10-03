4c329b6c8937e239988fad8b2c9d159ad134655c
Maurer Viktor Endre <maurer.viktor@bolyai.local>
2025-10-03 12:23:20 +0200

    Órai munka

---
+++ table_col/kitero.js
@@ -0,0 +1,44 @@
+console.log("Lóg a konzol!");
+
+/**
+ * A világ legjobb száma
+ * @type {int}
+ */
+let number = 6;
+
+console.log(number + number);
+
+/**
+ * A világ legjobb listája
+ */
+const array = ["ő", "lő", "elő", "velő", "évelő", "névelő"];
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);    
+}
+
+for (const item of array) {
+    console.log(item)
+}
+
+for (const key in array) {
+    console.log(key + ":" + array[key]);
+}
+
+for (const key in array) {
+console.log(`${key}:${array[key]}`);
+}
+
+/**
+ * A világ legjobb objektuma
+ * @type {{firstName:string, lastName:string, age:int}}
+ */
+const object = {
+    firstName : "Abdul",
+    lastName : "Allah",
+    age : 75
+};
+
+for (const key in object) {
+    console.log(object[key]);
+}
